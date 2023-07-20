e52f9a271b88e8534c7fcae74257472f5bfa15e6
obaedul islam <obaedul@redlimesolutions.com>
2023-07-20 13:09:51 +0600

    common scss added

--- header.php
+++ header.php
@@ -4,11 +4,22 @@
             <div class="col-md-3 col-sm-6 col-6">
                 <img src="images/header/logo.svg" alt="Ride Centric Logo">
             </div>
-            <div class="col-md-3 col-sm-6 col-6">
+            <div class="col-md-9 col-sm-6 col-6">
                 <ul class="navigation-menu">
                     <li><a href="">Sign Up</a></li>
                     <li><a href="">Sign In</a></li>
-                    <li><a id="toggleMenu" class="hamburger-menu"><span></span></a></li>
+                    <li><a id="toggleMenu" class="hamburger-menu"><span class="icon icon-bottle"></span></a></li>
+                    <ul id= "toggleMenuItem">
+                        <li> <a href="">Sign Up</a></li>
+                        <li> <a href="">Sign In</a></li>
+                        <li> <a href="">Home</a></li>
+                        <li> <a href="">Ride Reimagined</a></li>
+                        <li> <a href="">Fleet Exhibit</a></li>
+                        <li> <a href="">Peace Of Mind</a></li>
+                        <li> <a href="">Tech Savvy</a></li>
+                        <li> <a href="">Testimonials</a></li>
+                        <li> <a href="">Contact Us</a></li>
+                    </ul>
                 </ul>
             </div>
         </div>
