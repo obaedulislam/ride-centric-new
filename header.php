6e47c4f190e9ac270ce9cc4d985616ab2751b305
obaedul islam <obaedul@redlimesolutions.com>
2023-07-20 09:44:34 +0600

    structure done

--- header.php
+++ header.php
@@ -2,7 +2,14 @@
     <div class="container">
         <div class="row">
             <div class="col-md-3 col-sm-6 col-6">
-
+                <img src="images/header/logo.svg" alt="Ride Centric Logo">
+            </div>
+            <div class="col-md-3 col-sm-6 col-6">
+                <ul class="navigation-menu">
+                    <li><a href="">Sign Up</a></li>
+                    <li><a href="">Sign In</a></li>
+                    <li><a id="toggleMenu" class="hamburger-menu"><span></span></a></li>
+                </ul>
             </div>
         </div>
     </div>
